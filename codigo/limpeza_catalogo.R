99fcf022b640b6c571fab158ec8a4e1536ec99d5
marcosfanton <marcos.fanton@ufsm.br>
2023-05-24 08:57:54 -0300

    Limpeza de texto e padronização de variáveis. Correção do código anterior.

--- codigo/limpeza_catalogo.R
+++ codigo/limpeza_catalogo.R
@@ -6,9 +6,8 @@ library(textclean) #limpeza de texto
 library(genderBR) #rotulação de gênero com base no primeiro nome
 
 #Unificação dos bancos de 1987-2021####
-#Todos os bancos foram baixados em .csv na página da CAPES - Dados Abertos 
+#Todos os bancos foram baixados em CSV na página da CAPES - Dados Abertos 
 #e armazenados na pasta dados/catalogo. Ao todo, são 36 arquivos com cerca de 4.43GB
-#Importação dos arquivos .csvs
 #Bancos de 1987-2012
 banco8712 <- purrr::map_dfr(list.files(path = "dados/catalogo/capes8712", 
                                        pattern = "dados", 
@@ -25,8 +24,7 @@ banco1321 <- purrr::map_dfr(list.files(path = "dados/catalogo/capes1321",
                             na = c("NI", "NA"),
                             show_col_types = FALSE) 
 
-#Padronização e seleção das variáveis de cada banco para formação de banco único
-#Bancos de 1987-2012 - Ver Metadados do Catálogo
+#Variáveis dos Bancos de 1987-2012 - Ver Metadados do Catálogo
 vars8712 <- c("AnoBase",
               "NomeIes",
               "CodigoPrograma",
@@ -38,11 +36,10 @@ vars8712 <- c("AnoBase",
               "Regiao",
               "Uf",
               "AreaConhecimentoCodigo", 
-              "GrandeAreaDescricao",
               "NumeroPaginas",
               "ResumoTese")
 
-#Bancos de 2013-2021 - Ver Metadados do Catálogo
+#Variáveis dos Bancos de 2013-2021 - Ver Metadados do Catálogo
 vars1321 <- c("AN_BASE",
               "NM_ENTIDADE_ENSINO", 
               "CD_PROGRAMA",
@@ -55,13 +52,37 @@ vars1321 <- c("AN_BASE",
               "NM_REGIAO",
               "SG_UF_IES",
               "CD_AREA_CONHECIMENTO",   
-              "NM_GRANDE_AREA_CONHECIMENTO", 
               "NR_PAGINAS",
               "DS_RESUMO")
 
-#Junção dos bancos com as 15 variáveis escolhidas 
-catalogo8721 <- bind_rows(
-  banco8712 |> select(all_of(vars8712)) |> 
-    rename_with(.cols = all_of(vars8712), ~vars1321[vars1321 != "NM_GRAU_ACADEMICO"]), #Essa variável não se encontra nos bancos anteriores a 2013
+#Junção dos bancos com as 14 variáveis escolhidas 
+catalogo8721 <- dplyr::bind_rows(
+  banco8712 |> dplyr::select(all_of(vars8712)) |> 
+    dplyr::rename_with(.cols = all_of(vars8712), 
+                       ~vars1321[vars1321 != "NM_GRAU_ACADEMICO"]), #Essa variável não se encontra nos bancos anteriores a 2013
   banco1321) |> 
-  select(all_of(vars1321))
+  dplyr::select(all_of(vars1321)) |> 
+  dplyr::filter(CD_AREA_CONHECIMENTO == "70100004") #Inclusão apenas de trabalhos na área da filosofia
+
+#Salvar arquivo RAW -- CSV 
+catalogo8721 |>
+  readr::write_csv("dados/catalogo_raw.csv")
+
+#Limpeza do texto e padronização de variáveis
+catalogo8721 <- catalogo8721  |> 
+  dplyr::mutate(CD_PROGRAMA = as.factor(CD_PROGRAMA), # Torna variável como fator para não ser desconfigurada
+                dplyr::across(where(is.character), ~ { # Modifica apenas variáveis do tipo character
+             x <- stringr::str_to_lower(.) # Padroniza todo texto em caixa baixa
+             x <- stringi::stri_trans_general(x, "Latin-ASCII") # Transforma todo texto em Latin-ASCII ou expressões equivalente
+             x <- stringr::str_remove_all(x, "[[:punct:]]") # Remove pontuações
+             x <- stringr::str_remove_all(x, "[[:digit:]]") # Remove números
+             x <- stringr::str_squish(x) # Remove espaços consecutivos
+             x
+           }),
+           NM_SUBTIPO_PRODUCAO = str_replace_all(NM_SUBTIPO_PRODUCAO, # Padronização dos valores das variáveis
+                                        pattern = c("mestrado" = "dissertacao", 
+                                                    "doutorado" = "tese")),
+           NM_ENTIDADE_ENSINO = str_replace_all(NM_ENTIDADE_ENSINO, 
+                                       pattern = c("universidade federal da paraiba.*" = "universidade federal da paraiba",
+                                                   "universidade estadual do parana reitoria" = "universidade estadual do parana",
+                                                   ".*mesquita.*" = "universidade estadual paulista")))
